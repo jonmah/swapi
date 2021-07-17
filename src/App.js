154cf9da465e15a10e322998d2eeffcf0f882cf0
Jon Mah <jon@jonmah.com>
2021-07-17 16:13:39 -0700

    Added craco and configs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { NavBar } from "./components";
+import { NavBar } from "@components";
 
 function App() {
   return (
---
+++ src/config/aliases.js
@@ -0,0 +1,18 @@
+const aliases = (prefix = `src`) => ({
+  "@components": `${prefix}/components/`,
+  //   "@atoms": `${prefix}/components/atoms`,
+  //   "@molecules": `${prefix}/components/molecules`,
+  //   "@organisms": `${prefix}/components/organisms`,
+  //   "@templates": `${prefix}/components/templates`,
+  //   "@config": `${prefix}/config`,
+  //   "@enums": `${prefix}/enums`,
+  //   "@hooks": `${prefix}/hooks`,
+  //   "@icons": `${prefix}/components/atoms/Icons`,
+  //   "@styles": `${prefix}/styles`,
+  //   "@utils": `${prefix}/utils`,
+  //   "@state": `${prefix}/state`,
+  //   "@types": `${prefix}/types`,
+  //   "@storybookHelpers": `../.storybook/helpers`,
+});
+
+module.exports = aliases;
