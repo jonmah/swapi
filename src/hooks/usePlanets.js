fc441f002107ef64fea42c4b1606f50cb27f8f2a
Jon Mah <jon@jonmah.com>
2021-08-25 23:17:35 -0700

    Cleaned up usePlanets

--- src/hooks/usePlanets.js
+++ src/hooks/usePlanets.js
@@ -1,12 +1,14 @@
 import { useQuery } from "react-query"
 
+const fetchPlanets = async () => {
+  const data = await fetch("http://swapi.dev/api/planets/").then((res) =>
+    res.json()
+  )
+  return data
+}
+
 const usePlanets = () => {
-  const { data, isFetching, isError } = useQuery("planets", async () => {
-    const data = await fetch("http://swapi.dev/api/planets/").then((res) =>
-      res.json()
-    )
-    return data
-  })
+  const { data, isFetching, isError } = useQuery("planets", fetchPlanets)
 
   return { data, isError, isFetching }
 }
